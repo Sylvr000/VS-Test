c7a69290a307bc9d68ab3197884c273bef554f3e
jim.silverstein <jim@silversteins.net>
2016-12-15 16:43:22 -0600

    beer

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				Console.WriteLine("Hello world");
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"Exception: {ex}");
+			}
+			finally
+			{
+				Console.WriteLine("Enter to exit");
+				Console.ReadLine();
+			}
+		}
+	}
+}
